2af80ebade0e826c541fbdf4e6b2d28240512d35
xorua117 <xorua117@gmail.com>
2021-05-23 02:35:58 +0900

    modefiy signin and signup components

--- src/componenets/SignIn/SignIn.js
+++ src/componenets/SignIn/SignIn.js
@@ -1,46 +1,100 @@
 import React from 'react';
 import './SignIn.css';
 
-const SignIn = () => {
-    return (
-        <div className="signin">
-            <h1>Sign In</h1>
-            <hr className="signin-hr"/>
-            <div>
-                <input type="radio" name="radio-t" checked/>
-                <label htmlFor="Tutee"><b>Tutee</b></label>
-                <input type="radio" name="radio-t"/>
-                <label htmlFor="Tutor"><b>Tutor</b></label>
-            </div>
-            <div className="signin-form">
-                <div className="signin-label">
-                    <label htmlFor="email"><b>Email</b></label>
-                </div>
-                <input 
-                    className="signin-input"
-                    type="text" 
-                    placeholder="Enter Email" 
-                    name="email" 
-                    required 
-                />
-            </div>
-            <div className="signin-form">
-                <div className="signin-label">                    
-                    <label htmlFor="psw"><b>Password</b></label>
-                </div>
-                <input
-                    className="signin-input"
-                    type="password" 
-                    placeholder="Enter Password" 
-                    name="psw" 
-                    required 
-                />
-            </div>
-            <div>
-              <button type="submit" className="signin-btn">Sign In</button>
-            </div>    
-        </div>  
-    );
+class SignIn extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isSignInTutee: 'on',
+            isSignInTutor: 'off',
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+
+    onSignInTuteeCheckChange = (event) => {
+        this.setState({isSignInTutee: event.target.value})
+        this.setState({isSignInTutor: "off"})
+    }
+
+    onSignInTutorCheckChange = (event) => {
+        this.setState({isSignInTutor: event.target.value})
+        this.setState({isSignInTutee: "off"})
+    }
+
+    onSignInEmailChange = (event) => {
+        this.setState({signInEmail: event.target.value});
+    }
+
+    onSignInPasswordChange = (event) => {
+        this.setState({signInPassword: event.target.value});
+    }
+
+    onSubmitSignIn = () => {
+    }
+
+    render() {
+        return (
+            <div className="signin">
+                <h1>Sign In</h1>
+                <hr className="signin-hr"/>
+                <form>
+                    <div>
+                        <input 
+                            type="radio" 
+                            name="radio-t" 
+                            defaultChecked
+                            onChange={this.onSignInTuteeCheckChange}
+                        />
+                        <label htmlFor="Tutee"><b>Tutee</b></label>
+                        <input 
+                            type="radio" 
+                            name="radio-t"
+                            onChange={this.onSignInTutorCheckChange}
+                        />
+                        <label htmlFor="Tutor"><b>Tutor</b></label>
+                    </div>
+                    <div className="signin-form">
+                        <div className="signin-label">
+                            <label htmlFor="email"><b>Email</b></label>
+                        </div>
+                        <input 
+                            className="signin-input"
+                            type="text" 
+                            placeholder="Enter Email" 
+                            name="email" 
+                            required 
+                            onChange={this.onSignInEmailChange}
+                        />
+                    </div>
+                    <div className="signin-form">
+                        <div className="signin-label">                    
+                            <label htmlFor="psw"><b>Password</b></label>
+                        </div>
+                        <input
+                            className="signin-input"
+                            type="password" 
+                            placeholder="Enter Password" 
+                            name="psw" 
+                            required
+                            onChange={this.onSignInPasswordChange}
+                        />
+                    </div>
+                    <div>
+                        <button 
+                            type="reset" 
+                            className="signin-cancelbtn"
+                            >Reset</button>
+                        <button 
+                            type="submit" 
+                            className="signin-btn"
+                            onClick={this.onSubmitSignIn}
+                            >Sign In</button>
+                    </div>
+                </form>  
+            </div>  
+        );
+    }
 }
 
 export default SignIn;
--- src/componenets/SignUp/SignUp.js
+++ src/componenets/SignUp/SignUp.js
@@ -1,71 +1,120 @@
 import React from 'react';
 import './SignUp.css';
 
-const SignUp = () => {
-    return (
-        <div className="signup">
-            <h1>Sign Up</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr className="signup-hr"/>
-            <div>
-                <input type="radio" name="radio-t" checked/>
-                <label htmlFor="Tutee"><b>Tutee</b></label>
-                <input type="radio" name="radio-t"/>
-                <label htmlFor="Tutor"><b>Tutor</b></label>
-            </div>
-            <div className="signup-form">
-                <div className="signup-label">
-                    <label htmlFor="name"><b>Name</b></label>
-                </div>
-                <input 
-                    className="signup-input"
-                    type="text" 
-                    placeholder="Enter Name" 
-                    name="name" 
-                    required 
-                />
-            </div>
-            <div className="signup-form">
-                <div className="signup-label">
-                    <label htmlFor="email"><b>Email</b></label>
-                </div>
-                <input 
-                    className="signup-input"
-                    type="text" 
-                    placeholder="Enter Email" 
-                    name="email" 
-                    required 
-                />
-            </div>
-            <div className="signup-form">
-                <div className="signup-label">                    
-                    <label htmlFor="psw"><b>Password</b></label>
-                </div>
-                <input
-                    className="signup-input"
-                    type="password" 
-                    placeholder="Enter Password" 
-                    name="psw" 
-                    required 
-                />
-            </div>
-            <div className="signup-form">
-                <div className="signup-label">
-                    <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                </div>
-                <input 
-                    className="signup-input"
-                    type="password" 
-                    placeholder="Repeat Password" 
-                    name="psw-repeat" 
-                    required 
-                />
-            </div>
-            <div>
-              <button type="submit" className="signup-btn">Sign Up</button>
-            </div>    
-        </div>        
-    );
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isTutee: 'on',
+            isTutor: 'off',
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    onTuteeCheckChange = (event) => {
+        this.setState({isTutee: event.target.value})
+        this.setState({isTutor: "off"})
+    }
+
+    onTutorCheckChange = (event) => {
+        this.setState({isTutor: event.target.value})
+        this.setState({isTutee: "off"})
+    }
+
+    onNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    onSubmitSignUp = () => {
+    }
+
+    render() {
+        return (
+            <div className="signup">
+                <h1>Sign Up</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr className="signup-hr"/>
+                <form>
+                    <div>
+                        <input 
+                            type="radio" 
+                            name="radio-t" 
+                            defaultChecked
+                            onChange={this.onTuteeCheckChange}
+                        />
+                        <label htmlFor="Tutee"><b>Tutee</b></label>
+                        <input 
+                            type="radio" 
+                            name="radio-t"
+                            onChange={this.onTutorCheckChange}
+                        />
+                        <label htmlFor="Tutor"><b>Tutor</b></label>
+                    </div>
+                    <div className="signup-form">
+                        <div className="signup-label">
+                            <label htmlFor="name"><b>Name</b></label>
+                        </div>
+                        <input 
+                            className="signup-input"
+                            type="text" 
+                            placeholder="Enter Name" 
+                            name="name" 
+                            required 
+                            onChange={this.onNameChange}
+                        />
+                    </div>
+                    <div className="signup-form">
+                        <div className="signup-label">
+                            <label htmlFor="email"><b>Email</b></label>
+                        </div>
+                        <input 
+                            className="signup-input"
+                            type="text" 
+                            placeholder="Enter Email" 
+                            name="email" 
+                            required 
+                            onChange={this.onEmailChange}
+                        />
+                    </div>
+                    <div className="signup-form">
+                        <div className="signup-label">                    
+                            <label htmlFor="psw"><b>Password</b></label>
+                        </div>
+                        <input
+                            className="signup-input"
+                            type="password" 
+                            placeholder="Enter Password" 
+                            name="psw" 
+                            required 
+                            onChange={this.onPasswordChange}
+                        />
+                    </div>
+                    <div>
+                        <button 
+                            type="reset" 
+                            className="signup-cancelbtn"
+                        >Reset</button>
+                        <button 
+                            type="submit" 
+                            className="signup-btn"
+                            onClick={this.onSubmitSignUp}
+                        >Sign Up</button>
+                    </div>                    
+                </form>
+                    
+            </div>        
+        );
+    }
 }
 
 export default SignUp;
